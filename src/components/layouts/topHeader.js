3dd8845bf247e55693460ba5ab0b315668f2e6c4
EnkhErdene0421 <enkhgem@gmail.com>
2021-09-16 10:24:35 +0800

    first

---
+++ src/components/layouts/topHeader.js
@@ -0,0 +1,11 @@
+import { Col, Row } from "antd";
+import React from "react";
+
+const TopHeader = () => {
+  <header className="topHeader">
+    <Row>
+      <Col></Col>
+    </Row>
+  </header>;
+};
+export default TopHeader;
